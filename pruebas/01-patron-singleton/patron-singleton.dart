8b818121a40d6a1c70b59f4d97441560ecc76d48
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2025-06-29 13:32:23 -0500

    pruebas: patrón singleton

---
+++ pruebas/01-patron-singleton/patron-singleton.dart
@@ -0,0 +1,34 @@
+/// Pruebas personales: [Patrón Singleton]
+
+/*
+* ¿Qué es el patrón Singleton?
+* - El Singleton es un patrón de diseño creacional que:
+* - Restringe la creación de múltiples instancias de una clase.
+* - Garantiza una única instancia accesible globalmente.
+* - Se apoya en miembros static para almacenar esa instancia única.
+*
+* Ideal para cosas como:
+* - Configuraciones globales
+* - Gestores de recursos
+* - Conexiones de base de datos
+* - Clases que actúan como 'registro' o 'logger'
+*/
+
+class Logger {
+  static final Logger _instanciaUnica = Logger._interno();
+
+  // Constructor privado
+  Logger._interno();
+
+  // Método estático de acceso
+  static Logger get instancia => _instanciaUnica;
+
+  void log(String mensaje) {
+    print('🪵 $mensaje');
+  }
+}
+
+void main() {
+  Logger.instancia.log('Primer log');
+  Logger.instancia.log('Segundo log');
+}
