149e8ff233b0cec1f18876c5d248b19b6bf7ee0c
Blue Feather Dev <jesusdominguez2004@gmail.com>
2025-07-07 13:25:14 -0500

    my-modules: get superclass & used mixins

---
+++ my-modules/analyzeClassMixins.dart
@@ -0,0 +1,42 @@
+/// Get Superclass & used Mixins from a Class Base
+/// Using: [dart:mirrors]
+
+import 'dart:mirrors';
+
+class MixinAnalysis {
+  final String? superclass;
+  final List<String> mixins;
+
+  const MixinAnalysis({this.superclass, required this.mixins});
+
+  @override
+  String toString() =>
+    'Superclass: $superclass\nMixins: ${mixins.isEmpty ? 'None' : mixins.join(', ')}';
+}
+
+MixinAnalysis analyzeClassMixins(Type type) {
+  final mirror = reflectClass(type);
+  final typeChain = <String>[];
+
+  ClassMirror? current = mirror.superclass;
+
+  // Extract automatically generated type chain
+  while (current != null && current.reflectedType.toString().startsWith('_')) {
+    typeChain.add(current.reflectedType.toString());
+    current = current.superclass;
+  }
+
+  // Nominal superclass (outside the confusing mixins)
+  final superClass = current?.reflectedType.toString();
+
+  // Find the compound name generated
+  final fullName = typeChain.firstWhere(
+    (name) => name.contains('&'),
+    orElse: () => '',
+  );
+
+  final parts = fullName.split('&');
+  final mixins = parts.length > 2 ? parts.sublist(2) : <String>[];
+
+  return MixinAnalysis(superclass: superClass, mixins: mixins);
+}
